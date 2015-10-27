8c74bed17f34687493a05e79f801a3340ae41cf7
Redge Ballard <redge.ballard@gmail.com>
2015-10-27 14:37:16 -0600

    Do the mongo thing

--- node/server.js
+++ node/server.js
@@ -5,31 +5,31 @@ var readline = require('readline');
 var ROOT_DIR = "html/";
 http.createServer(function (req, res) {
   var urlObj = url.parse(req.url, true, false);
-  console.log("URL path "+urlObj.pathname);
-  console.log("URL search "+urlObj.search);
-  console.log("URL query "+urlObj.query["q"]);
-  if(urlObj.pathname.indexOf("getcity") !=-1) {
-    var myRe = new RegExp("^"+urlObj.query["q"]);
-    console.log(myRe);
-    console.log("query is ",urlObj.query["q"]);
-    var jsonresult = [];
-    fs.readFile('cities.dat.txt', function (err, data) {
-      if(err) throw err;
-      cities = data.toString().split("\n");
-      for(var i = 0; i < cities.length; i++) {
-        var result = cities[i].search(myRe);
-        if(result != -1) {
-          console.log(cities[i]);
-          jsonresult.push({city:cities[i]});
-        }
-      }
-      console.log(jsonresult);
-      console.log(JSON.stringify(jsonresult));
-      res.writeHead(200);
-      res.end(JSON.stringify(jsonresult));
-    });
+  // If this is our comments REST service
+  if(urlObj.pathname.indexOf("comment") !=-1) {
+    console.log("comment route");
+    if(req.method === "POST") {
+      console.log("POST comment route");
+      var jsonData = "";
+      req.on('data', function (chunk) {
+        jsonData += chunk;
+      });
+      req.on('end', function () {
+        var reqObj = JSON.parse(jsonData);
+        console.log(reqObj);
+        console.log("Name: "+reqObj.Name);
+        console.log("Comment: "+reqObj.Comment);
+        var MongoClient = require('mongodb').MongoClient;
+        MongoClient.connect("mongodb://localhost/weather", function(err, db) {
+          if(err) throw err;
+          db.collection('comments').insert(reqObj,function(err, records) {
+            console.log("Record added as "+records[0]._id);
+          });
+        });
+      });
+    }
   } else {
-    // Normal static file
+   // Normal static file
     fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
       if (err) {
         res.writeHead(404);
@@ -41,12 +41,3 @@ http.createServer(function (req, res) {
     });
   }
 }).listen(4000);
-
-console.log("Listening on port 4000");
-
-
-var options = {
-    hostname: 'localhost',
-    port: '4000',
-    path: 'news.html'
-  };
